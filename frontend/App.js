7de52e8f0d5bef467db378c9ba9b5d466f3253d2
Nicky Chen <nickychen2022@gmail.com>
2024-01-19 22:16:02 -0500

    Added frontend

--- frontend/App.js
+++ frontend/App.js
@@ -49,7 +49,7 @@ export default function App() {
           setSteakType={setSteakType}
         />
       )}
-      {/* <StatusBar style="auto" /> */}
+      <StatusBar style="auto" />
     </View>
   );
 }
